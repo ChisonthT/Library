ca22c2ed6289af45033fb507cdc57b72bac7baef
Chisonth <1533494+ChisonthT@users.noreply.github.com>
2024-12-06 18:39:34 -0500

    Added a 'add a book' button. Completed step 4

--- script.js
+++ script.js
@@ -34,7 +34,72 @@ function displayLibrary(){
     };
 };
 
+function clearLibraryDisplay(){
+    let table = document.getElementById("myTable");
 
+    let i = mylibrary.length;
+    while(i > 1){
+        table.deleteRow(-1);
+        i--;
+    }
+}
+
+
+function userBooks(){
+    let title;
+    let author;
+    let pages;
+    let read;
+
+    let text = "";
+    let input = prompt("Please enter the title of the book:", "");
+
+    while (input == null || input == "") {
+        input = prompt("Please enter the title of the book:", "");
+    }
+
+    title = input;
+
+    input = "";
+
+    while (input == null || input == "") {
+        input = prompt("Please enter the author of the book:", "");
+    }
+
+    author= input;
+
+    input = "";
+
+    while (input == null || input == "") {
+        input = prompt("Please enter the page number of the book:", "");
+    }
+
+    pages = input;
+
+    input = "";
+
+    while (input == null || input == "") {
+        input = prompt("Please enter whether the book has been read or not:", "");
+    }
+
+    read = input;
+
+    input = "";
+
+    let book1 = new Book(title,author,pages,read);
+
+    addBookToLibrary(book1);
+    clearLibraryDisplay();
+    displayLibrary();
+    
+}
+
+
+
+
+
+
+//Testing the functions
 let book1 = new Book("The Hobbit", "JRR Tolkien", 295, "yes");
 let book2 = new Book("The Lord of the Rings", "JRR Tolkien", 294, "yes");
 let book3 = new Book("The Silmarillion", "JRR Tolkien", 294, "yes");
